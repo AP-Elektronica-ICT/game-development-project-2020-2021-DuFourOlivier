15e3960ab7d932b0d89b98598e8cf046cc10868e
Olivier <Olivier.dufour@student.ap.be>
2020-11-13 17:18:07 +0100

    Refactor PlayerFrames

--- GameDev_Olivier_DuFour_2EACL1/GameDev_Olivier_DuFour_2EACL1/Animation/AnimatiePlayer.cs
+++ GameDev_Olivier_DuFour_2EACL1/GameDev_Olivier_DuFour_2EACL1/Animation/AnimatiePlayer.cs
@@ -25,6 +25,30 @@ namespace GameDev_Olivier_DuFour_2EACL1.Animation
             framesIdleLeft = new List<AnimationFrame>();
             counter = 0;
             frameMovement = 0;
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(756, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(648, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(540, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(432, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(324, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(216, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(108, 140, 108, 140)));
+            AddFrameWalkLeft(new AnimationFrame(new Rectangle(0, 140, 108, 140)));
+
+
+            //NAar rechts
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(0, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(108, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(216, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(324, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(432, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(540, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(648, 0, 108, 140)));
+           AddFrameWalkRight(new AnimationFrame(new Rectangle(756, 0, 108, 140)));
+            //IdleRight
+           AddFrameIdleRight(new AnimationFrame(new Rectangle(0, 0, 108, 140)));
+
+            //IdleLeft
+           AddFrameIdleLeft(new AnimationFrame(new Rectangle(756, 140, 108, 140)));
         }
 
         public void AddFrameWalkRight(AnimationFrame animationFrame)
--- GameDev_Olivier_DuFour_2EACL1/GameDev_Olivier_DuFour_2EACL1/Player.cs
+++ GameDev_Olivier_DuFour_2EACL1/GameDev_Olivier_DuFour_2EACL1/Player.cs
@@ -24,33 +24,6 @@ namespace GameDev_Olivier_DuFour_2EACL1
         {
             playerTexture = text;
             animatie = new AnimatiePlayer();
-            //naar links
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(756, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(648, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(540, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(432, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(324, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(216, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(108, 140, 108, 140)));
-            animatie.AddFrameWalkLeft(new AnimationFrame(new Rectangle(0, 140, 108, 140)));
-
-            
-            //NAar rechts
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(0, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(108, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(216, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(324, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(432, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(540, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(648, 0, 108, 140)));
-            animatie.AddFrameWalkRight(new AnimationFrame(new Rectangle(756, 0, 108, 140)));
-            //IdleRight
-            animatie.AddFrameIdleRight(new AnimationFrame(new Rectangle(0, 0, 108, 140)));
-
-            //IdleLeft
-            animatie.AddFrameIdleLeft(new AnimationFrame(new Rectangle(756, 140, 108, 140)));
-
-
             Position = new Vector2(10, 365);
             this.inputReader = reader;
             moveCommand = new MoveCommand();
